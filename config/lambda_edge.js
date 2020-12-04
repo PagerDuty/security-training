57bb9a64dca4f0b568fa47b4dc99161f7a06bd8a
Rich Adams <git@richadams.me>
2020-12-04 13:00:15 -0800

    Update CSP header to fix Google Analytics

--- config/lambda_edge.js
+++ config/lambda_edge.js
@@ -1,4 +1,4 @@
-// Node.js 6.10 Lambda @ Edge
+// Node.js Lambda @ Edge
 // Adds security headers to static site hosted in CloudFront (sudo.pagerduty.com)
 // https://aws.amazon.com/blogs/networking-and-content-delivery/adding-http-security-headers-using-lambdaedge-and-amazon-cloudfront/
 
@@ -10,7 +10,7 @@ exports.handler = (event, context, callback) => {
 
     // Set new headers
     headers['strict-transport-security'] = [{key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains'}];
-    headers['content-security-policy'] = [{key: 'Content-Security-Policy', value: "default-src 'none'; script-src 'self' ssl.google-analytics.com www.google-analytics.com 'sha256-nOjM72OI0W7QRbH5RI3KgGucBgLF9Njy4jlVYaUnkmA=' 'sha256-fX9qfHwuLRAhhgRM/UaGciy4GcC8DMLAacNFMsVHnk8=' 'sha256-aWflYUKL1Gj8gU1eM4Mhgu1d5OH6qY8vRRpLE1QFUF8='; object-src 'self'; style-src 'self' 'sha256-LpfmXS+4ZtL2uPRZgkoR29Ghbxcfime/CsD/4w5VujE=' 'sha256-nK6A3vwzvwoN92MnHZrWtylYkYmW1jCQgTqWKQJNBMI=' 'sha256-QVPYhFew7P3p46DxC68MMy/iAdzPaXWHAFcS3O2A0tI='; img-src 'self' data: ssl.google-analytics.com www.google-analytics.com; media-src 'none'; frame-src 'none'; font-src 'self'; connect-src 'self'; base-uri 'none'; form-action 'self'; frame-ancestors 'none'"}];
+    headers['content-security-policy'] = [{key: 'Content-Security-Policy', value: "default-src 'none'; script-src 'self' ssl.google-analytics.com www.google-analytics.com 'sha256-nOjM72OI0W7QRbH5RI3KgGucBgLF9Njy4jlVYaUnkmA=' 'sha256-fX9qfHwuLRAhhgRM/UaGciy4GcC8DMLAacNFMsVHnk8=' 'sha256-aWflYUKL1Gj8gU1eM4Mhgu1d5OH6qY8vRRpLE1QFUF8='; object-src 'self'; style-src 'self' 'sha256-LpfmXS+4ZtL2uPRZgkoR29Ghbxcfime/CsD/4w5VujE=' 'sha256-nK6A3vwzvwoN92MnHZrWtylYkYmW1jCQgTqWKQJNBMI=' 'sha256-QVPYhFew7P3p46DxC68MMy/iAdzPaXWHAFcS3O2A0tI='; img-src 'self' data: ssl.google-analytics.com www.google-analytics.com; media-src 'none'; frame-src 'none'; font-src 'self'; connect-src 'self' ssl.google-analytics.com www.google-analytics.com; base-uri 'none'; form-action 'self'; frame-ancestors 'none'"}];
     headers['x-content-type-options'] = [{key: 'X-Content-Type-Options', value: 'nosniff'}];
     headers['x-frame-options'] = [{key: 'X-Frame-Options', value: 'DENY'}];
     headers['x-xss-protection'] = [{key: 'X-XSS-Protection', value: '1; mode=block'}];
